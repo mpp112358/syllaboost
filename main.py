9176385fc4a5febc11c927c9a4ce3680e763593f
Manuel Pérez <mpp12358@icloud.com>
2025-12-22 23:08:26 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from syllaboost!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ syllabooster/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SyllaboosterConfig(AppConfig):
+    name = "syllabooster"
